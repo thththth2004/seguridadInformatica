109fe9a30dd248564ed48d16caf7bb3797d767d4
JoakoV3 <joaquinvarelaivan@gmail.com>
2021-10-21 17:22:31 -0300

    primer commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export const Nav = () => {
+  return (
+    <div>
+      <h2>Seguridad Informatica</h2>
+      <div>
+        <Link>Malwares</Link>
+        <Link>Prevencion</Link>
+        <Link>Cifrado</Link>
+      </div>
+    </div>
+  )
+}
